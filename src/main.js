4cae662036d27809b6f7f2230857032392f2d205
jonson <luizlindonjonson@yahoo.com.br>
2020-06-27 20:44:09 -0300

    criando um servidor

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log ('olá');
+import { createServer } from 'http';
+
+const server = createServer();
+
+server.listen(8000, '127.0.0.1', ()=>{
+    console.log('Server is listening at http://127.0.0.1:8000.');
+});
