fa79f23830dd35ca3531545db1ef4764fd8458fd
jonson <luizlindonjonson@yahoo.com.br>
2020-06-27 21:39:06 -0300

    mandando um JSON como resposta.

--- src/main.js
+++ src/main.js
@@ -4,13 +4,19 @@ const server = createServer((request, response) => {
     switch (request.url) {
         case '/status': {
 
-            response.writeHead(200);
-            response.write('<h1> OKS </h1>');
+            response.writeHead(200, {
+                'Content-Type': 'application/json',
+            });
+            response.write(
+                JSON.stringify({
+                    status: '<h1> OKS </h1>',
+                })
+            );
             response.end();
             break;
         }
-        default:{
-            response.writeHead(404, 'Service not found. ');            
+        default: {
+            response.writeHead(404, 'Service not found. ');
             response.end();
         }
     }
@@ -18,6 +24,6 @@ const server = createServer((request, response) => {
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
-server.listen(8000, '127.0.0.1', () => {
+server.listen(PORT, HOSTNAME, () => {
     console.log(`Server is listening at http://${HOSTNAME}:${PORT}.`);
 });
